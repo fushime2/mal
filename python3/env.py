70194b56283dc305c3c9aad4787379a7a3906d4a
fushime2 <okmt52@gmail.com>
2018-11-19 22:52:40 +0900

    add step3

---
+++ python3/env.py
@@ -0,0 +1,17 @@
+"""
+hold the environment definition
+"""
+
+class Env():
+    def __init__(self):
+        self.outer = 0
+        self.data = {}
+
+    def set(self):
+        pass
+
+    def find(self):
+        pass
+
+    def get(self):
+        pass
